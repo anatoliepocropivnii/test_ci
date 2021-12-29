02ef4f37d4e140833a5a75496ad432cc0f839f15
anatoliepocropivnii <90709463+anatoliepocropivnii@users.noreply.github.com>
2021-12-29 10:31:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 set -e
+
 printenv
-set
+printenv
+printenv
+printenv
+printenv
+
 CODE_PATH="$1"
-echo ===== $CODE_PATH
-#. $IDF_PATH/export.sh
+
+. $IDF_PATH/export.sh
 
 cd "${CODE_PATH}"
 
-#idf.py build
+idf.py build
 
-test CMD
-$2
-"$2"
+
+#!/usr/bin/env bash
+#set -e
+
+#CODE_PATH="$1"
+
+#. $IDF_PATH/export.sh
+
+#cd "${CODE_PATH}"
+
+#idf.py build
